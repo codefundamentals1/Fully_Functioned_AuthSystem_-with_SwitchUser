9089557124f3083eeaf6edd5e130f6b5da970918
codefundamentals1 <anishr888999@gmail.com>
2025-09-27 07:10:47 +0530

    fixed

--- Frontend/src/static/validusers.js
+++ Frontend/src/static/validusers.js
@@ -3,17 +3,17 @@ const validusers =
     {
         "uid": "1",
         "username": "anish",
-        "password": "$2b$10$0url/25foO1XCDqNUTBabuabE5NDLoCiZKAPyKJGYSOsoOizYwSsS"
+        "password": "anish123"
     },
     {
         "uid": "69",
         "username": "AR",
-        "password": "$2b$10$8TXGQ2.R9.lIwWdKZgXxZOyffdPHfrm3n./EJIw0qzWrIXssUjESe"
+        "password": "anish123"
     },
     {
         "uid": "69",
         "username": "abi",
-        "password": "$2b$10$quA790LH0Xbl7gvrIuVrJO7D6cMMnmw/PdtQsq3eCMODWcpKwkRqy"
+        "password": "anish123"
     }
 ]
 //pass word is anish123
